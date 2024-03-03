0b88427bce052c724e53124a1f2ab455d1352d5c
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-03-03 19:31:59 +0100

    Add styles
    Create static/css/styles.css
    Set STATICFILES_DIRS
    Load static in base.html and link css/styles.css

--- project/settings.py
+++ project/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / "static" ] # Add this line to include the static folder in the project directory
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
