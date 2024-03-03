31711e919bca872c7b61d8b78d132f6e912fc5b4
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-03-03 11:35:37 +0100

    Add signup page
    Create templates/registration/signup.html
    Create the view to the signup page
    Add a signup button to the home page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login") # Redirect to the login page after a successful registration
+    template_name = "registration/signup.html" # The template used to render the page
