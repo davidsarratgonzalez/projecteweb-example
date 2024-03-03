a42fd82d86bfcbfb942b5c455e7db3c9298943bf
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-03-03 11:20:09 +0100

    Add login page
    Include built-in links to the project URLs
    Create templates/registration/login.html
    Set the LOGIN_REDIRECT_URL to the home page
    We can now add a link to the login page in the home page

--- project/settings.py
+++ project/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home" # Add this line to redirect the user to the home page after logging in
--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),  # Create a new path for the home page
+    path("accounts/", include("django.contrib.auth.urls")),  # Add the path for the built-in authentication URLs
 ]
